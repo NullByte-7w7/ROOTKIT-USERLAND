1ac20bd8ccc1d651de022b308854823693e4240a
DARKSEC <121623691+DARKSECshell@users.noreply.github.com>
2024-04-26 22:36:34 -0300

    write.c

---
+++ write.c
@@ -0,0 +1,36 @@
+// hookando a syscall da write()
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <dlfcn.h>
+
+        ssize_t write(int fd, const void *buf, size_t count)
+{
+
+
+        ssize_t (*darksec)(int fd, const void *buf, size_t count);
+
+        darksec = dlsym(RTLD_NEXT, "write");
+
+if(strcmp(buf, "darksec") == 0){
+
+        return write(1, "Hacked 1337", 12);
+
+} else {
+
+        return darksec(fd, buf, count);
+}
+
+}
+
+int main(void){
+
+        char nome[200];
+
+        printf("Qual Seu Nome? \n");
+        scanf("%s", &nome);
+
+        write(1, nome, strlen(nome));
+
+}
